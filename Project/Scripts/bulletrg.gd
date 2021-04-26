0709dcfa87dc5ce71505ab1af3b8a2883a780186
bruhrator420 <n3cr0jr@gmail.com>
2021-04-26 23:13:21 +0100

    Added a Railgun

---
+++ Project/Scripts/bulletrg.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+const SPEED = 100
+var direction = Vector2()
+var velocity = 1
+
+func _physics_process(delta):
+	
+	direction.x -= SPEED * delta - velocity
+	
+	translate(direction)
+	
+	if position.x <= -1000:
+		queue_free()
+		print("lolokeskrr")
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Scrub": 
+		body._death()
---
+++ Project/Scripts/railgun.gd
@@ -0,0 +1,29 @@
+extends StaticBody2D
+
+var bullet = preload("res://Scenes/bulletrg.tscn")
+var bulletCount = 0
+var canfire = true
+
+var Timer = 0
+
+func _physics_process(delta):
+	
+	Timer += 1 
+	
+	if Timer > 50: 
+		Timer = 0
+		bulletCount = 0
+		
+	if Timer < 50 and bulletCount < 1: 
+		
+		var b = bullet.instance()
+		if canfire == true:
+			get_parent().add_child(b)
+			b.global_position = $Position2D.global_position
+			bulletCount += 1
+			$Timer2.start()
+			canfire = false
+
+
+func _on_Timer2_timeout():
+	canfire = true
