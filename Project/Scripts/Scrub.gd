ee59105ef6a85ceb1bcab5b95426624a435b81d6
TigreBranco <80484750+TigreBranco@users.noreply.github.com>
2021-05-13 17:40:21 +0100

    Added a comment

--- Project/Scripts/Scrub.gd
+++ Project/Scripts/Scrub.gd
@@ -7,8 +7,7 @@ var direction = 1
 var cooldownnotactive = true
 var hasfireball = false
 var is_jumping = false
-
-
+#Onready Variables
 onready var Coyote_Timer = $CoyoteTimer
 onready var Jump_Buffer = $JumpBuffer
 #Constants
