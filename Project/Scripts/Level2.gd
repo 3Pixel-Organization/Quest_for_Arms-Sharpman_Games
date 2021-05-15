b7069a7e377298e893f2f7fe1ddc75e3a3dbf5fe
bruhrator420 <n3cr0jr@gmail.com>
2021-05-15 12:13:18 +0100

    Makes the Fireball shootable in the beggining of Level 2

--- Project/Scripts/Level2.gd
+++ Project/Scripts/Level2.gd
@@ -4,3 +4,8 @@ extends Node2D
 func _on_Scrub_death():
 	get_node("HUD/Node2D").visible()
 	$AudioStreamPlayer.stop()
+
+
+func _on_Area2D2_body_entered(body):
+	if body.name == "Scrub":
+		body.fireball_pickup()
