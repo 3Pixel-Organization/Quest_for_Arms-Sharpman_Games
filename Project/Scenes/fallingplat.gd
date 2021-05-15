cc53f923bb58d07c138b03c3285a1a6d8fe6deac
bruhrator420 <n3cr0jr@gmail.com>
2021-05-15 14:22:00 +0100

    Added a Falling Platform + A New Area in Level 2

---
+++ Project/Scenes/fallingplat.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+var iscrumbling = false 
+
+func _ready():
+	pass
+
+
+
+
+
+
+
+func _on_Timer_timeout():
+	$AnimationPlayer.play("New Anim")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	iscrumbling = false
+	queue_free()
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Scrub" && !iscrumbling:
+		iscrumbling = true
+		$AnimationPlayer2.play("New Anim")
+		$Timer.start()
