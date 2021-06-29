6b2e8e89a0ba785c45d4e1b5f0890f62323c3923
bruhrator420 <n3cr0jr@gmail.com>
2021-06-29 13:10:18 +0100

    Skip Cutscene Option

--- Project/Scripts/level1-level2.gd
+++ Project/Scripts/level1-level2.gd
@@ -6,3 +6,7 @@ extends VideoPlayer
 
 func _on_VideoPlayer_finished():
 	get_tree().change_scene("res://Scenes/Level2.tscn")
+
+func _process(delta):
+	if Input.is_action_pressed("ui_accept"):
+		get_tree().change_scene("res://Scenes/Level2.tscn")
