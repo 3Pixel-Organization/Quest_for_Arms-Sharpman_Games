4426daf6ed8758a33aa454ae68dc40ba32a78d86
Rúben Ribeiro Franco <rubenfranco009@gmail.com>
2021-09-11 20:57:05 +0100

    Scale level 2 and add the tileset
    Everything needs to be repositioned into conrrect positions, but everything should work

--- Project/Scripts/Level2.gd
+++ Project/Scripts/Level2.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
+onready var scrub = $Scrub
+
+func _ready():
+	if scrub.has_method("fireball_pickup") && !scrub.has_fireball:
+		scrub.fireball_pickup()
+
 func _on_Scrub_death():
 	get_node("HUD/Node2D").visible()
-	$AudioStreamPlayer.stop()
-
-func _on_Area2D2_body_entered(body):
-	if body.name == "Scrub":
-		body.fireball_pickup()
+	$music.stop()
 
 func _enter_tree():
 	if Checkpoint.laast_location:
