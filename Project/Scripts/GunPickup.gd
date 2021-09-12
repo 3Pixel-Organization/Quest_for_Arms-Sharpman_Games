b955812618e4e46ac51df41ffb9d0ab7be0e24f1
Rúben Ribeiro Franco <rubenfranco009@gmail.com>
2021-09-12 14:22:31 +0100

    GunPickup now checks body's functions instead of it's name

--- Project/Scripts/GunPickup.gd
+++ Project/Scripts/GunPickup.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 func _on_gun_picked_up(body):
-	if body.name == "Scrub":
+	if body.has_method("fireball_pickup"):
 		body.fireball_pickup()
 		$Gun.queue_free()
 		$CollisionShape2D.queue_free()
