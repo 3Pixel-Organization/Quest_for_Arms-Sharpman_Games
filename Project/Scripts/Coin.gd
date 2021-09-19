875011ee1c74125a5956df5612ed7bd0156e4af4
Rúben Ribeiro Franco <rubenfranco009@gmail.com>
2021-09-19 17:51:45 +0100

    Improve Coin

--- Project/Scripts/Coin.gd
+++ Project/Scripts/Coin.gd
@@ -1,14 +1,12 @@
 extends Area2D
 
-signal coin_collected
-
-## Coin collection
+# Coin collection
 func _on_Coin_body_entered(body):
-	if body.name == "Scrub":
+	if body is ScrubPlayer:
+		body.coins += 1
 		$AnimationPlayer.play("Bounce")
-		emit_signal("coin_collected")
 		set_collision_layer_bit(0,false)
 		$Sound.play()
 
-func _on_AnimationPlayer_animation_finished(_anim_name):
+func _on_Sound_finished():
 	queue_free()
