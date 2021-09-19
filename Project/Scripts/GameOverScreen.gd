4d7d9462b83aa9bd0c008fd1470ac5d62caa3e28
Rúben Ribeiro Franco <rubenfranco009@gmail.com>
2021-09-19 15:47:14 +0100

    Update gameplay HUD elements
    HUD elements now use containers instead of manual anchoring.
    The pause screen is now it's own scene.
    Scripts use by HUD elemens have been reorganized.

---
+++ Project/Scripts/GameOverScreen.gd
@@ -0,0 +1,24 @@
+extends ColorRect
+
+onready var restart_button = $"MarginContainer/VBoxContainer/Buttons/Restart"
+onready var quit_button = $"MarginContainer/VBoxContainer/Buttons/Quit"
+onready var menu_button = $"MarginContainer/VBoxContainer/Buttons/Menu"
+onready var death_jingle = $DeathJingle
+
+
+func _on_player_death():
+	visible = true
+	death_jingle.play()
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
+
+
+func _on_Restart_pressed():
+# warning-ignore:return_value_discarded
+	get_tree().reload_current_scene()
+
+
+func _on_Menu_pressed():
+		get_tree().change_scene("res://Scenes/MainMenu.tscn")
--- Project/Scripts/PauseScreen.gd
+++ Project/Scripts/PauseScreen.gd
@@ -1,21 +1,28 @@
 extends Control
 
+onready var resume_button = $"CenterContainer/VBoxContainer/Buttons/Resume"
+onready var quit_button = $"CenterContainer/VBoxContainer/Buttons/Quit"
+
+
 func _process(_delta):
 	if Input.is_action_pressed("Pause"):
 		visible = true
 		get_tree().paused = true
 
-func _on_Button_pressed():
+
+func _on_Resume_pressed():
 	hide()
 	get_tree().paused = false
 
-func _on_Button2_pressed():
-	get_tree().paused = false
+
+func _on_Quit_pressed():
 	get_tree().quit()
 
-func _on_Button3_pressed():
+
+func _on_Menu_pressed():
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://Scenes/MainMenu.tscn")
+
 
 func _on_HSlider_value_changed(value):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
