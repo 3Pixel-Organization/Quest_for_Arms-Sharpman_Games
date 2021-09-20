8229be4ce8ba9b47dfa1d11b69437fba0dc802e4
Rúben Ribeiro Franco <rubenfranco009@gmail.com>
2021-09-20 22:45:38 +0100

    Modify falling platform behavior

--- Project/Scripts/fallingplat.gd
+++ Project/Scripts/fallingplat.gd
@@ -1,14 +1,36 @@
 extends KinematicBody2D 
 
-func _on_FallTimer_timeout():
-	$AnimationPlayer.play("Fall")
-	$CollisionShape2D.disabled = true
+var velocity: Vector2 = Vector2()
 
-func _on_AnimationPlayer_animation_finished(_anim_name):
-	queue_free()
+onready var animation_player := $"AnimationPlayer"
+onready var shake_timer := $"ShakeTimer"
+onready var visibility_notifier := $"VisibilityNotifier2D"
+onready var top_checker := $"TopChecker"
+
+
+func _ready():
+	set_physics_process(false)
+
+
+func _physics_process(delta):
+# warning-ignore:return_value_discarded
+	move_and_collide(velocity * delta)
+
 
 func _on_TopChecker_body_entered(body):
-	if body.name == "Scrub":
-		$TopChecker/CollisionShape2D.disabled = true
-		$AnimationPlayer.play("Shake")
-		$FallTimer.start()
+	if body is ScrubPlayer:
+		top_checker.set_deferred("monitoring", false)
+		animation_player.play("Shake")
+		var err = visibility_notifier.connect("screen_exited", self, "_on_screen_exited")
+		assert(err == OK)
+		shake_timer.start()
+
+
+func _on_ShakeTimer_timeout():
+	$CollisionShape2D.disabled = true
+	set_physics_process(true)
+	velocity.y = 500
+
+
+func _on_screen_exited():
+	queue_free()
