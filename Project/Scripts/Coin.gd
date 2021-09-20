a69222d6813bbcd145d1314ad8d2ca9075b0a8dd
Rúben Ribeiro Franco <rubenfranco009@gmail.com>
2021-09-20 23:14:52 +0100

    Improve coin's animation and code.

--- Project/Scripts/Coin.gd
+++ Project/Scripts/Coin.gd
@@ -5,7 +5,7 @@ func _on_Coin_body_entered(body):
 	if body is ScrubPlayer:
 		body.coins += 1
 		$AnimationPlayer.play("Bounce")
-		set_collision_layer_bit(0,false)
+		set_deferred("monitoring", false)
 		$Sound.play()
 
 func _on_Sound_finished():
