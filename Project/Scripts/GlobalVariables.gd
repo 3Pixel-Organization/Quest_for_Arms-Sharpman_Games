e2d0d04ff4e4f9e24f6f5f85e4c6bbf381e65b86
Rúben Ribeiro Franco <rubenfranco009@gmail.com>
2021-09-28 00:08:23 +0100

    Replace checkpoint system with full global variables
    Remember to rename checkpoint.gd to Checkpoint.gd since NTFS doesn't distinguish from lowercase and uppercase file names.

---
+++ Project/Scripts/GlobalVariables.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var checkpoint_location: Vector2
+var player = {
+	"Gun": true,
+	"Coins": 0,
+	}
--- Project/Scripts/checkpoint.gd
+++ Project/Scripts/checkpoint.gd
@@ -1,6 +1,8 @@
-extends Node
-
-var laast_location = null
+extends Area2D
+class_name CheckpointArea
 
 func _ready():
-	pass
+	connect("body_entered", self, "_on_Area2D_body_entered")
+
+func _on_Area2D_body_entered(_body):
+	GlobalVariables.checkpoint_location = global_position
