0b96d80589da45562456f2d5d8d07382fc25eba4
Rúben Ribeiro Franco <rubenfranco009@gmail.com>
2021-10-14 00:25:58 +0100

    Update level 1
    Removed unnecessary script;
    The enemies now walk to the defaut direction (left);
    Move the tilemap and the coins a little bit;
    Set correct collision layers for various object in the level.

--- Project/Scripts/EndLevelPortal.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-func _on_EndLevel_body_entered(body):
-	if body is ScrubPlayer:
-# warning-ignore:return_value_discarded
-		get_tree().change_scene("res://Scenes/level1-level2.tscn")
--- Project/Scripts/Level1.gd
+++ Project/Scripts/Level1.gd
@@ -3,5 +3,12 @@ extends Node
 func _ready():
 	get_tree().paused = false
 
+
 func _on_player_death():
 	$"Music".stop()
+
+
+func _on_EndLevelPortal_body_entered(body):
+	if body is ScrubPlayer:
+# warning-ignore:return_value_discarded
+		get_tree().change_scene("res://Scenes/level1-level2.tscn")
