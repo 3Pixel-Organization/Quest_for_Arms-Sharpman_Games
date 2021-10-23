80bccfd310bcbbeedec480fb12b97ccd341d3b8d
Rúben Ribeiro Franco <rubenfranco009@gmail.com>
2021-10-23 16:55:05 +0100

    Add direction variable to cannon

--- Project/Scripts/Cannon.gd
+++ Project/Scripts/Cannon.gd
@@ -1,15 +1,26 @@
+tool
 extends StaticBody2D
+
+enum DIRECTION {
+	LEFT = -1,
+	RIGHT = 1,
+}
 
 export var projectile_scene: PackedScene
 export var projectile_speed: float
+export(DIRECTION) var direction: int = DIRECTION.LEFT
 
 # Needs to be onready otherwise gets assigned to nothing
 onready var projectile: Resource = load(projectile_scene.resource_path)
+onready var sprite: Sprite = $"Sprite"
 
+
+func _ready():
+	scale.x = direction * -1
 
 func _on_Clock_timeout():
 	if $"VisibilityEnabler2D".is_on_screen():
 		var projectile_instance = projectile.instance()
 		projectile_instance.global_position = $"ProjectileOrigin".global_position
-		projectile_instance.velocity.x = -projectile_speed
+		projectile_instance.velocity.x = projectile_speed * direction
 		get_tree().current_scene.add_child(projectile_instance)
