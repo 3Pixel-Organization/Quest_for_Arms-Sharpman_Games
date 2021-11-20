3718f737ae1869da6623632a88478567f0fe21f8
bruhrator420 <n3cr0jr@gmail.com>
2021-11-20 20:24:39 +0000

    Limit the Skipping of the startup cutscene to a few inputs.

--- Project/Scripts/startup.gd
+++ Project/Scripts/startup.gd
@@ -5,5 +5,6 @@ func _on_VideoPlayer_finished():
 	assert(get_tree().change_scene("res://Scenes/MainMenu.tscn") == OK)
 
 
-func _input(_event):
-	assert(get_tree().change_scene("res://Scenes/MainMenu.tscn") == OK)
+func _input(event):
+	if Input.is_action_pressed("skip"):
+		get_tree().change_scene("res://Scenes/MainMenu.tscn")
