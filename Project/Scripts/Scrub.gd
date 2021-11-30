b88185a18a96a5181368467a0af6a6b500ae4527
bruhrator420 <n3cr0jr@gmail.com>
2021-11-30 13:20:18 +0000

    Change Scrub's movement parameters.

--- Project/Scripts/Scrub.gd
+++ Project/Scripts/Scrub.gd
@@ -8,7 +8,7 @@ signal death
 const MAX_SPEED = 100
 const ACCELERATION = MAX_SPEED * 7
 const JUMP_SPEED = -175
-const GRAVITY = 300
+const GRAVITY = 325
 const MAX_FALL_SPEED = MAX_SPEED * 10
 const FIREBALL = preload("res://Scenes/fireball.tscn")
 const JUMPPAD_SPEED = -200
