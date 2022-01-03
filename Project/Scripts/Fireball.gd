64e93647c0cc425ae464f4381c23e57710689e46
Rúben Ribeiro Franco <rubenfranco009@gmail.com>
2022-01-03 22:12:06 +0000

    Make the fireball an actual physics object

--- Project/Scripts/Fireball.gd
+++ Project/Scripts/Fireball.gd
@@ -1,29 +1,29 @@
-extends Area2D # Must be upgraded to a RigidBody2D
-
-var cooldowninaction = false
-var velocity = Vector2()
+extends KinematicBody2D
 
 
-func _ready():
+var velocity = Vector2() # Must be set by instaciator
+
+
+func _ready() -> void:
 	if not velocity:
 		queue_free()
-	$Sprite.flip_h = velocity.x < 0
-
-
-func _physics_process(delta):
-	global_translate(velocity * delta)
-
-
-func desintegrate():
-	queue_free()
-
-
-func _on_VisibilityNotifier2D_screen_exited():
-	queue_free()
-
-
-func _on_Fireball_body_entered(body: Node2D):
-	if body.has_method("damage"):
-		body.damage()
 	
-	desintegrate()
+	($"Sprite" as Sprite).flip_h = velocity.x < 0
+
+
+func _physics_process(delta) -> void:
+	var collision_data := move_and_collide(velocity * delta) as KinematicCollision2D
+	if collision_data:
+		var colliding_body := collision_data.collider as CollisionObject2D
+		if colliding_body and colliding_body.has_method("damage"):
+			colliding_body.damage()
+		
+		desintegrate()
+
+
+func desintegrate() -> void:
+	queue_free()
+
+
+func _on_VisibilityNotifier2D_screen_exited() -> void:
+	queue_free()
