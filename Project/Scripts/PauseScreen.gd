656d2276f11ce3efe4b7aa7dbad50ccd0ad17e0c
Rúben Ribeiro Franco <rubenfranco009@gmail.com>
2022-01-23 22:43:51 +0000

    Fix pausescreen working in the middle of the death animation

--- Project/Scripts/PauseScreen.gd
+++ Project/Scripts/PauseScreen.gd
@@ -3,25 +3,29 @@ extends Control
 
 onready var resume_button: Button = $"CenterContainer/VBoxContainer/Buttons/Resume"
 onready var quit_button: Button = $"CenterContainer/VBoxContainer/Buttons/Quit"
+onready var tree: SceneTree = get_tree()
 
+func _input(event: InputEvent) -> void:
+	if not event.is_action_pressed("Pause"): return
 
-func _process(_delta: float) -> void:
-	if Input.is_action_pressed("Pause"):
-		visible = true
-		get_tree().paused = true
+	visible = true
+	pause_mode = PAUSE_MODE_PROCESS
+	tree.paused = true
+	tree.set_input_as_handled()
 
 
 func _on_Resume_pressed() -> void:
-	hide()
-	get_tree().paused = false
+	visible = false
+	pause_mode = PAUSE_MODE_STOP
+	tree.paused = false
 
 
 func _on_Quit_pressed() -> void:
-	get_tree().quit()
+	tree.quit()
 
 
 func _on_Menu_pressed() -> void:
-	var scene_changed: int = get_tree().change_scene("res://Scenes/MainMenu.tscn")
+	var scene_changed: int = tree.change_scene("res://Scenes/MainMenu.tscn")
 	assert(scene_changed == OK, "Scene not found at given path")
 
 
