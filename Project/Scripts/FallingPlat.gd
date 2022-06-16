d49023d18d80467bf3bba1cc5b52585db47cb8ed
Rúben Ribeiro Franco <rubenfranco009@gmail.com>
2022-06-16 13:13:37 +0100

    Make falling platform respawn after 5 seconds
    Also clean up it's code a bit.

--- Project/Scripts/FallingPlat.gd
+++ Project/Scripts/FallingPlat.gd
@@ -1,16 +1,18 @@
 extends KinematicBody2D 
 
+var original_pos: Vector2 = Vector2.ZERO
 
 export var falling_velocity: Vector2 = Vector2(0, 500)
 
 onready var animation_player: AnimationPlayer = $"AnimationPlayer"
 onready var shake_timer: Timer = $"ShakeTimer"
-onready var visibility_notifier: VisibilityNotifier2D = $"VisibilityNotifier2D"
+onready var respawn_timer: Timer = $"RespawnTimer"
 onready var top_checker: Area2D = $"TopChecker"
 
 
 func _ready() -> void:
 	set_physics_process(false)
+	original_pos = global_position
 
 
 func _physics_process(delta: float) -> void:
@@ -23,15 +25,18 @@ func _on_TopChecker_body_entered(body: ScrubPlayer ) -> void:
 	
 	top_checker.set_deferred("monitoring", false)
 	animation_player.play("Shake")
-	var err: int = visibility_notifier.connect("screen_exited", self, "_on_screen_exited")
-	assert(err == OK)
-	shake_timer.start()
+	shake_timer.start() # Maybe should just queue shake like 4 times
+	respawn_timer.start()
 
 
 func _on_ShakeTimer_timeout() -> void:
-	($"CollisionShape2D" as CollisionShape2D).disabled = true
+	animation_player.stop()
+	($"CollisionShape2D" as CollisionShape2D).set_deferred("disabled", true)
 	set_physics_process(true)
 
 
-func _on_screen_exited() -> void:
-	queue_free()
+func _on_RespawnTimer_timeout() -> void:
+	set_physics_process(false)
+	top_checker.set_deferred("monitoring", true)
+	($"CollisionShape2D" as CollisionShape2D).set_deferred("disabled", false)
+	global_position = original_pos
