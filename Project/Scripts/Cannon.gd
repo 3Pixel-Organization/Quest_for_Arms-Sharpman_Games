4e9f96866bb2498a4bbf4a0edcedcc68f033d8bb
RRFGamer <rubenfranco009@gmail.com>
2022-07-05 17:53:55 +0100

    Add option to use detection areas for cannon activation
    Example included in level 3

--- Project/Scripts/Cannon.gd
+++ Project/Scripts/Cannon.gd
@@ -10,13 +10,35 @@ export var projectile_scene: PackedScene
 export var projectile_speed: float
 export(DIRECTION) var direction: int = DIRECTION.LEFT
 
-# Needs to be onready otherwise gets assigned to nothing
 onready var projectile: Resource = load(projectile_scene.resource_path)
 onready var sprite: Sprite = $"Sprite"
+onready var clock: Timer = $"Clock"
+onready var detection_area: Area2D = get_node_or_null("DetectionArea")
 
 
 func _ready() -> void:
+	set_process(false)
+	if detection_area:
+		
+		var connected := detection_area.connect("body_entered", self, "_clock_start") as int
+		assert(connected == OK)
+		
+		connected = detection_area.connect("body_exited", self, "_clock_stop") as int
+		assert(connected == OK)
+		
+		clock.stop()
+	
 	scale.x = direction * -1
+
+
+func _clock_start(_body: ScrubPlayer) -> void:
+	clock.start()
+
+
+func _clock_stop(_body: ScrubPlayer) -> void:
+	if detection_area.get_overlapping_bodies().size() <= 0:
+		clock.stop()
+
 
 func _on_Clock_timeout() -> void:
 	if $"VisibilityEnabler2D".is_on_screen():
