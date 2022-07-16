04fc97a8a1218dd35d24d8e49e90a2953a1d59df
RRFGamer <rubenfranco009@gmail.com>
2022-07-16 19:33:04 +0100

    Move cutscene code to checkpoint's script

--- Project/Scripts/Checkpoint.gd
+++ Project/Scripts/Checkpoint.gd
@@ -1,12 +1,17 @@
 extends Area2D
 
 
+export var play_cutscene: bool = false
+export var cutscene_name: String = "LevelOverview"
+
 onready var level: Node = get_tree().current_scene
+onready var index: int = get_index()	# Does not allow for checkpoints
+										# created dynamically
 
 
 func _ready() -> void:
 	if (GlobalVariables.checkpoint["Level"] == level.name and
-			get_index() <= GlobalVariables.checkpoint["Index"]):
+			index <= GlobalVariables.checkpoint["Index"]):
 		return
 	
 	var checkpoint_ready: int = connect("body_entered", self,
@@ -15,6 +20,15 @@ func _ready() -> void:
 			# shouldn't trigger more than once
 	
 	assert(checkpoint_ready == OK, "Checkpoint collision signal couldn't connect")
+	
+	if not play_cutscene:
+		return
+	
+	var cutscene_ready: int = connect("body_entered", level,
+			"_on_CutsceneCheckpoint_body_entered", [cutscene_name],
+			CONNECT_ONESHOT)
+	
+	assert(cutscene_ready == OK, "Cutscene collision signal couldn't connect")
 
 
 func _on_Area2D_body_entered(body: ScrubPlayer) -> void:
@@ -24,7 +38,7 @@ func _on_Area2D_body_entered(body: ScrubPlayer) -> void:
 	GlobalVariables.checkpoint = {
 		"Level": level.name,
 		"Position": global_position,
-		"Index": get_index(),
+		"Index": index,
 	}
 	
 	GlobalVariables.player = {
