3Pixel-Organization/Quest_for_Arms-Sharpman_Games

commit b2b070d26e5cf88ac1cb785dd078476452023f5a

Clean up code Change one line if statements into multiline Set correct collision layers and masks Correctly capitalize and privatize some function Delete default environment as it's not needed

Project/Scripts/Scrub.gd
@@ -1,10 +1,11 @@
 class_name ScrubPlayer
 extends KinematicBody2D
 
+
 # Signals
 signal death(wait_time)
 
-enum DIRECTION {
+enum Direction {
 	LEFT = -1,
 	RIGHT = 1,
 }
@@ -17,12 +18,12 @@ const GRAVITY = 325
 const MAX_FALL_SPEED = MAX_SPEED * 10
 const FIREBALL = preload("res://Scenes/Fireball.tscn")
 
-export(DIRECTION) var direction: int = DIRECTION.RIGHT
+export(Direction) var direction: int = Direction.RIGHT
 export var can_die: bool = true
 export var velocity: Vector2 = Vector2.ZERO
 
 # Member variables
-var coins: int setget set_coins
+var coins: int setget _set_coins
 var has_fireball: bool = GlobalVariables.player["Gun"]
 var events: Dictionary = {
 	"right": [],
@@ -45,7 +46,7 @@ onready var melee_area: Area2D = $"Kick"
 onready var hud_coins: Label = $"HUD/GameHUD/PanelContainer/HSplitContainer/Coins"
 
 # Camera and Tween are optional; They're only used for the death zoom
-onready var camera := get_node_or_null("Camera2D")
+onready var camera: Camera2D = get_node_or_null("Camera2D")
 onready var tween: Tween = get_node_or_null("Tween")
 
 
@@ -58,15 +59,17 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	#print(jump_buffer.wait_time, " ", jump_buffer.time_left)
 	# Store Inputs
-	var jump: bool = Input.is_action_just_pressed("jump")                     # UP, W or spacebar
-	var shoot_fireball: bool = Input.is_action_just_pressed("shoot_fireball") # Q
-	var kick: bool = Input.is_action_just_pressed("kick")                     # X
-	var new_x_speed: float = Input.get_axis("left", "right")                  # A and D
+	# Jump - Up arrow key, W or Spacebar
+	# Shoot fireball - Q
+	# Kick - X
+	# Horizontal movement - Left and Right arrow keys, or A and D
+	var jump: bool = Input.is_action_just_pressed("jump")
+	var shoot_fireball: bool = Input.is_action_just_pressed("shoot_fireball")
+	var kick: bool = Input.is_action_just_pressed("kick")
+	var new_x_speed: float = Input.get_axis("left", "right")
 	
-	velocity.y = min(velocity.y + GRAVITY * delta,
-			MAX_FALL_SPEED)
+	velocity.y = min(velocity.y + GRAVITY * delta, MAX_FALL_SPEED)
 	
 	var is_on_floor: bool = is_on_floor()
 	
@@ -111,8 +114,6 @@ func _physics_process(delta: float) -> void:
 	# oposite to the current direction
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
-	
-	animator.playback_speed = 1
 	
 	if animator.current_animation != "Attack":
 		if velocity.y:
@@ -171,11 +172,14 @@ func clear_default_input_events() -> void:
 		InputMap.action_erase_events(action)
 
 
-func readd_events_to_actions(_wait_time: float = 0) -> void:	# wait_time is needed because the
-	for action in events.keys():								# death signal calls methods with it
+func readd_events_to_actions(_wait_time: float = 0) -> void:
+	# wait_time is needed because the
+	# death signal calls methods with it
+	for action in events.keys():
 		for event in events[action]:
 			InputMap.action_add_event(action, event)
 			# Should clear events var??
+
 
 func release_all_actions() -> void:
 	for action in events.keys():
@@ -197,7 +201,7 @@ func _on_Kick_body_entered(body: PhysicsBody2D) -> void:
 func _on_GunCooldown_timeout() -> void:
 	mounted_gun.frame = 0
 
-# Setters and getters
-func set_coins(value: int) -> void:
+
+func _set_coins(value: int) -> void:
 	coins = value
 	hud_coins.text = (coins as String).pad_zeros(2)

Project/Scenes/Cannon.tscn
@@ -9,12 +9,15 @@ extents = Vector2( 12, 12.5 )
 
 [node name="Cannon" type="StaticBody2D"]
 collision_layer = 6
+collision_mask = 0
 script = ExtResource( 2 )
 projectile_scene = ExtResource( 3 )
 projectile_speed = 300.0
 
 [node name="VisibilityEnabler2D" type="VisibilityEnabler2D" parent="."]
 rect = Rect2( -17, -27, 34, 27 )
+process_parent = true
+physics_process_parent = true
 
 [node name="Sprite" type="Sprite" parent="."]
 position = Vector2( 0, -13 )

Project/Scenes/Door.tscn
@@ -52,7 +52,7 @@ animations = [ {
 extents = Vector2( 56, 24 )
 
 [node name="Door" type="Area2D"]
-collision_layer = 0
+collision_layer = 4
 monitorable = false
 
 [node name="KinematicBody2D" type="KinematicBody2D" parent="."]

Project/Scenes/GunPickup.tscn
@@ -40,6 +40,7 @@ hue_variation_random = 1.0
 
 [node name="GunPickup" type="Area2D"]
 collision_layer = 32
+collision_mask = 0
 monitorable = false
 script = ExtResource( 2 )
 
@@ -72,4 +73,4 @@ anims/Bob = ExtResource( 4 )
 
 [node name="Timer" type="Timer" parent="."]
 
-[connection signal="body_entered" from="." to="." method="_on_gun_picked_up"]
+[connection signal="body_entered" from="." to="." method="_on_Gun_picked_up" flags=6]

Project/Scripts/Cannon.gd
@@ -1,35 +1,36 @@
 extends StaticBody2D
 
 
-enum DIRECTION {
+enum Direction {
 	LEFT = -1,
 	RIGHT = 1,
 }
 
 export var projectile_scene: PackedScene
 export var projectile_speed: float
-export(DIRECTION) var direction: int = DIRECTION.LEFT
+export(Direction) var direction: int = Direction.LEFT
 
 onready var projectile: Resource = load(projectile_scene.resource_path)
 onready var sprite: Sprite = $"Sprite"
 onready var clock: Timer = $"Clock"
+onready var projectile_origin: Position2D = $"ProjectileOrigin"
 onready var detection_area: Area2D = get_node_or_null("DetectionArea")
 
-
 func _ready() -> void:
-	set_process(false)
 	if detection_area:
 		
-		var connected := detection_area.connect("body_entered", self, "_clock_start") as int
+		var connected := detection_area.connect("body_entered", self,
+				"_clock_start") as int
 		assert(connected == OK)
 		
-		connected = detection_area.connect("body_exited", self, "_clock_stop") as int
+		connected = detection_area.connect("body_exited", self,
+				"_clock_stop") as int
 		assert(connected == OK)
 		
 		clock.stop()
 	
 	sprite.flip_h = direction > 0
-	$"ProjectileOrigin".position.x *= -direction
+	projectile_origin.position.x *= -direction
 
 
 func _clock_start(_body: ScrubPlayer) -> void:
@@ -44,7 +45,7 @@ func _clock_stop(_body: ScrubPlayer) -> void:
 func _on_Clock_timeout() -> void:
 	if $"VisibilityEnabler2D".is_on_screen() or detection_area:
 		var projectile_instance = projectile.instance()
-		projectile_instance.global_position = $"ProjectileOrigin".global_position
+		projectile_instance.global_position = projectile_origin.global_position
 		projectile_instance.velocity.x = projectile_speed * direction
 		projectile_instance.spawner = self
 		get_tree().current_scene.add_child(projectile_instance)

Project/Scripts/CannonBall.gd
@@ -12,15 +12,15 @@ func _ready() -> void:
 	if not velocity:
 		queue_free()
 	else:
-		sprite.flip_h = clamp(sign(velocity.x), 0, 1) as bool
+		sprite.flip_h = velocity.x > 0
 
 
 func _physics_process(delta) -> void:
-	var collisionData: KinematicCollision2D = move_and_collide(velocity * delta)
+	var collisionData := move_and_collide(velocity * delta) as KinematicCollision2D
 	if collisionData:
 		if collisionData.collider is ScrubPlayer:
 			collisionData.collider.die()
-		if not collisionData.collider == spawner:
+		if collisionData.collider != spawner:
 			desintegrate()
 
 

Project/Scripts/Checkpoint.gd
@@ -5,7 +5,7 @@ onready var level: Node = get_tree().current_scene
 
 
 func _ready() -> void:
-	if (GlobalVariables.checkpoint["Level"] == get_tree().current_scene.name and
+	if (GlobalVariables.checkpoint["Level"] == level.name and
 			get_index() <= GlobalVariables.checkpoint["Index"]):
 		return
 	
@@ -14,7 +14,7 @@ func _ready() -> void:
 			# The signal in oneshot because the checkpoint
 			# shouldn't trigger more than once
 	
-	assert(checkpoint_ready == OK, "Checkpoint collision signal not connected")
+	assert(checkpoint_ready == OK, "Checkpoint collision signal couldn't connect")
 
 
 func _on_Area2D_body_entered(body: ScrubPlayer) -> void:

Project/Scripts/Coin.gd
@@ -1,11 +1,18 @@
 extends Area2D
 
+onready var animation_player: AnimationPlayer = $"AnimationPlayer"
+onready var sound: AudioStreamPlayer = $"Sound"
+
 
 func _on_Coin_body_entered(body: ScrubPlayer) -> void:
+	if not body:
+		return
+	
 	body.coins += 1
-	($"AnimationPlayer" as AnimationPlayer).play("Bounce")
+	animation_player.play("Bounce")
 	set_deferred("monitoring", false)
-	($"Sound" as AudioStreamPlayer).play()
+	sound.play()
+
 
 func _on_Sound_finished() -> void:
 	queue_free()

commit 24be6bf7ddfa95b22ca4686534afdc0423efbda2

Correctly set the playback speed in scrub's script Scrub's death animation should no longer stop at the start b2b070d2 introduced a regression that extended the effect to any animation player after the player came to a complete stop.

Project/Scripts/Scrub.gd
@@ -118,9 +118,9 @@ func _physics_process(delta: float) -> void:
 	if animator.current_animation != "Attack":
 		if velocity.y:
 			animator.play("Jump")
-		elif velocity.x:
+		elif new_x_speed:
 			animator.play("Walk")
-			animator.playback_speed = new_x_speed
+			animator.playback_speed = abs(new_x_speed)
 		else:
 			animator.play("Idle")
 